dfc03e00fb96ad575f618d42cc080a2f887e766e
sarahleightaal <270141828@yoobeestudent.ac.nz>
2022-08-11 14:54:41 +1200

    changes to server

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require("express")
 const app = express()
 
 app.listen(3000)
-//anon function - request and result
-app.get("/", (req, res) => (
-    console.log("abc")
-    res.send("something")
-))
+
+app.get("/", (req, res) => {
+    console.log("hello")
+})
